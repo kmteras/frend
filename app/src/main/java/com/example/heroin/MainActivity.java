d588c51d1540697ac0acb93a3476b3086387b899
Karl Martin Teras <karl.martin@ter.as>
2019-10-12 17:43:48 +0300

    Format

--- app/src/main/java/com/example/heroin/MainActivity.java
+++ app/src/main/java/com/example/heroin/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.heroin;
 
-import android.app.ActivityManager;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
@@ -54,10 +48,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     /**
      * USING BORROWED CODE FROM: https://www.androidhive.info/2016/05/android-working-with-card-view-and-recycler-view/?utm_source=recyclerview&utm_medium=site&utm_campaign=refer_article
-     *
+     * <p>
      * RecyclerView item decoration - give equal margin around grid item
      */
     public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {

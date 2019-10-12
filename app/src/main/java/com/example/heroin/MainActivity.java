622e72e0066ae7a11120b373c58941b182b25a16
Sergei Kuštšenko <sergei.kustsenko@gmail.com>
2019-10-12 13:54:31 +0300

    Button checking

--- app/src/main/java/com/example/heroin/MainActivity.java
+++ app/src/main/java/com/example/heroin/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.heroin;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,11 +21,37 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.startButton);
 
+        startForegroundService(new Intent(MainActivity.this, Overlay.class));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setTurnScreenOn(true);
+        }
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startForegroundService(new Intent(MainActivity.this, Overlay.class));
+
             }
         });
+    }
+    @Override public boolean onKeyDown ( int keyCode, KeyEvent event){
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Log.d("Test", "Back button pressed!");
+            return false;
+        } else if (keyCode == KeyEvent.KEYCODE_HOME || keyCode == KeyEvent.KEYCODE_MOVE_HOME) {
+            Log.d("Test", "Home button pressed!");
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onUserLeaveHint()
+    {
+        Log.d("onUserLeaveHint","User left the app");
+        super.onUserLeaveHint();
     }
 }

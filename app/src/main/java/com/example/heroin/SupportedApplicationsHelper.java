58e5bf0ae940b4c9d7190c1f75ac5f5df622f40d
Tiit Kass <dyyd.tk@gmail.com>
2019-10-13 16:06:17 +0300

    Added kids drawing app to hardcoded supported apps for demo

--- app/src/main/java/com/example/heroin/SupportedApplicationsHelper.java
+++ app/src/main/java/com/example/heroin/SupportedApplicationsHelper.java
@@ -8,11 +8,12 @@ public class SupportedApplicationsHelper {
     public static final List<String> SUPPORTED_APPS = Arrays.asList(
 //            "com.google.android.apps.photos",
             "com.hmdglobal.camera2",
-            "com.google.android.apps.books",
-            "com.google.android.calculator",
+//            "com.google.android.apps.books",
+//            "com.google.android.calculator",
             "com.google.android.music",
 //            "com.google.android.videos",
-            "com.google.android.play.games"
+            "com.google.android.play.games",
+            "com.ng_labs.kidspaint"
     );
 
     public static List<String> getListOfSupportedApplications() {
